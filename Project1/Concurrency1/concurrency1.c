599f66e60070c12d86de0c7946e01f46ad00225c
Austin <nguyenau@oregonstate.edu>
2017-04-18 16:59:04 -0700

    started concurrency

---
+++ Project1/Concurrency1/concurrency1.c
@@ -0,0 +1,12 @@
+/* Hello this is Austin working on concurrency for CS444 */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <rand.h>
+
+using namespace std;
+
+pthread_mutex_t lock;
+
