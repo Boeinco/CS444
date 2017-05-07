3bcaee294809a2176bda36629c12a041ef10f706
Isaac Stallcup <stallcui@oregonstate.edu>
2017-05-07 12:16:32 -0700

    finished io test script

--- Project2/scripts/iotest.sh
+++ Project2/scripts/iotest.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 
+num_trees=$1
+
 rm -rf testdir
+
+num_write=0
+
+sectors=()
+fmt=("\t"$num_write"\n")
+
+sectors+=$(du ~ | tail -n1 | awk '{print $1}' 2>&1)
+fmt=("\t\t"$num_write"\n")
+sectors+=$fmt
 
 mkdir testdir
 cd testdir
 
-for i in `seq 0 8`; do
+sectors+=$(du ~ | tail -n1 | awk '{print $1}' 2>&1)
+num_write=$((num_write + 1))
+fmt=("\t\t"$num_write"\n")
+sectors+=$fmt
+
+for i in `seq 0 $num_trees`; do
 	mkdir test$i
 	cd test$i
-	for j in `seq 0 8`; do
-		echo "Isaac is the best!" > test"$j".txt
-		du ~/testdir | tail -n1 | awk '{print $1}'
+	for j in `seq 0 $num_trees`; do
+		cat ~/hamlet.txt > test"$j".txt #copies the full text of Hamlet
+		sectors+=$(du ~ | tail -n1 | awk '{print $1}' 2>&1)
+		num_write=$(($num_write + 1))
+		fmt=("\t\t"$num_write"\n")
+		sectors+=$fmt
+
 	done
 	cd ..
 done
 
-for i in `seq 0 8`; do
-	cd test$i
-	for j in `seq 0 8`; do
-		filename=test"$i".txt
+cd ..
 
-		B=$(hdparm --fibmap $filename | grep 'sectors' -A 1 | tail -n1 | awk '{print $4}')
-		fmt=" has size "
-		echo $filename$fmt$B
-	done
-	cd ..
-done
+rm -rf testdir
+
+sectors+=$(du ~ | tail -n1 | awk '{print $1}' 2>&1)
+num_write=$(($num_write + 1))
+fmt=("\t\t"$num_write"\n")
+sectors+=$fmt
+
+echo -e "Sectors Used \t I/O Operations" > sectors.txt
+echo -e $sectors >> sectors.txt
+
+cat sectors.txt
