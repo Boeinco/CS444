12dc4f7d6a529bfe88db78e768be7d3d6e6a6027
Austin <nguyenau@oregonstate.edu>
2017-05-17 12:09:45 -0700

    new version uploaded

--- Project3/concurrency/concurrency3.c
+++ Project3/concurrency/concurrency3.c
@@ -1,176 +1,168 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <signal.h>
 #include "mt19937ar.c"
 
-struct entry
-{
-	int num;
-	struct entry* next;
+struct linkedList list;
+int eax, ebx, ecx, edx;
+int searchers, inserters, deleters, num;
+
+pthread_mutex_t smutex;
+pthread_mutex_t imutex;
+pthread_mutex_t dmutex;
+
+struct linkedList {
+    int val;
+    struct linkedList *next;
 };
+struct linkedList *head;
 
-struct buffer
-{
-	struct entry* head;
-	struct entry* current;
-	int counter;
-};
+int randnum(int low, int high) {
+    int i = 0;
+    i = (int)genrand_int32();
+    i = abs(i);
+    i %= (high - low);
+    if (i < low) {
+        i = low;
+    }
 
-struct buffer buff;
-int num;
-int deleters, searchers, inserters;
-
-void checker(int signal)
-{
-	kill(0,signal);
-	exit(0);
+    return i;
 }
 
-int twisterrand(int high, int low)
-{
-	int i = 0;
-	i = (int) genrand_int32();
-	i = abs(i);
-	i %= (high-low);
-	if (i < low)
-	{
-		i = low;
-	}
+
+int getLinkedListSize() {
+    struct linkedList *size;
+    size = head;
+    int i = 0;
+    while (size != NULL) {
+        size = size->next;
+        i++;
+    }
 	return i;
 }
 
-void searcher()
-{
-	int i;
-	struct entry *start = buff.head;
+void *searcher() {
+    struct linkedList *searchlist;
 
-	while (deleters != 0)
-	{
-		sleep(1);
-	}
-	printf("Searcher searching!\n");
-	searchers++;
-	if(num < buff.counter)
-	{
-		printf("Node not found.\n");
-		return;
-	}
-
-	for (i = 0; i < num; i++)
-	{
-		start = start->next;
-	}
-	printf("Num is %d\n", start->num);
-	searchers--;
-}
-
-void inserter()
-{
-	struct entry *item;
-	printf("Item is being inserted now\n");
-
-	while(deleters != 0)
-	{
-		sleep(1);
-	}
-	while(inserters != 0)
-	{
-		sleep(1);
-	}
-	inserters = 1;
-	item.num = twisterrand(100,1);
-	while(buff.current->next != NULL)
-	{
-		buff.current = buff.current->next;
-	}
-	buff.current->next = &item;
-	buff.current = buff.current->next;
-	buff.current->next = NULL;
-	buff.counter++;
-	printf("Item added!\n");
-	inserters = 0;
-}
-
-void deleter()
-{
-	int i;
-	struct entry *del = buff.head;
-	struct entry *temp;
-
-	while(deleters != 0)
-	{
-		sleep(1);
-	}
-	deleters = 1;
-	while (inserters != 0)
-	{
-		sleep(1);
-	}
-	while (searchers != 0)
-	{
-		sleep(1);
-	}
-	printf("deleters is deleting\n");
-
-	if(num == 1)
-	{
-		printf("Only head can be deleted so not deleting it\n");
-		return;
-	}
-	else if(num < buff.counter)
-    {
-        printf("No node\n");
-    }
-    for(i = 0; i < num; i++)
-    {
-        if(i == (num - 2))
-        {
-            temp = del;
+    while (1) {
+        if (!pthread_mutex_trylock(&smutex)) {
+            searchlist = head;
+            if (searchlist == NULL) {
+                printf("List is empty\n");
+                continue;
+            } else {
+                while (searchlist != NULL) {
+                    printf("%d ", searchlist->val);
+                    searchlist = searchlist->next;
+                }
+                printf("\n");
+            }
+            pthread_mutex_unlock(&smutex);
         }
-        del = del->next;          
+        sleep(1);
     }
- 
-    temp->next = del->next;
-    free(del);
-    printf("Done\n");
-    deleters = 0;
+}
+void *inserter() {
+    int randomnum;
+    struct linkedList *insertlist, **tail;
+
+    while (1) {
+        if (getLinkedListSize() < 25) {
+            if (!pthread_mutex_trylock(&imutex)) {
+
+                randomnum = randnum(1, 10);
+                insertlist = (struct linkedList *)malloc(sizeof(struct linkedList));
+
+                printf("Insert val: %d\n", randomnum);
+                insertlist->val = randomnum;
+                insertlist->next = NULL;
+                tail = &head;
+
+                if (head == NULL) {
+                    head == insertlist;
+                } else {
+                    while (*tail != NULL) {
+                        tail = &((*tail)->next);
+                    }
+                    *tail = insertlist;
+                }
+                pthread_mutex_unlock(&imutex);
+                sleep(1);
+            }
+        }
+    }
 }
 
-int main()
-{
-	struct sigaction signal;
-	struct entry initial;
-	pthread_t t1, t2, t3, t4;
-	void *d = deleter;
-	void *i = inserter;
-	void *s = searcher;
+void *deleter() {
+    int deleteNode;
+    struct linkedList *deletelist, *previous;
 
-	sigemptyset(&signal.sa_mask);
-	signal.sa_flags = 0;
-	sig.sa_handler = sig_catch;
-	sigaction(SIGINT, &signal, NULL);
+    while (1) {
 
-	initial.next = NULL;
-	initial.num = 1;
-	buff.head = &initial;
+        if (getLinkedListSize() > 1) {
 
-	deleters = 0;
-	searchers = 0;
-	inserters = 0;
-	number = 2;
+             if (!pthread_mutex_trylock(&imutex)) {
+                if (!pthread_mutex_trylock(&smutex)) {
 
-	pthread_create(&t1, NULL, i, NULL);
-    pthread_create(&t2, NULL, i, NULL);
-    pthread_create(&t3, NULL, s, NULL);
-    pthread_create(&t4, NULL, d, NULL);
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    pthread_join(t3, NULL);
-    pthread_join(t4, NULL);
+                    deletelist = head;
 
-	while(true){
-	}
+                    deleteNode = randnum(1, 10);
+
+                    while (deletelist != NULL) {
+
+                        if (deletelist->val == deleteNode) {
+                            printf("Delete val: %d\n", deleteNode);
+
+                            if (deletelist == head) {
+                                head = deletelist->next;
+                                free(deletelist);
+                                break;
+                            } else {
+                                previous->next = deletelist->next;
+                                free(deletelist);
+                                break;
+                            }
+                        } else {
+                            previous = deletelist;
+                            deletelist = deletelist->next;
+                        }
+                    }
+
+                    pthread_mutex_unlock(&smutex);
+                }
+
+                pthread_mutex_unlock(&imutex);
+            }
+            
+            sleep(1);
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+    pthread_t searchThread[3], insertThread[3], deleteThread[3];
+	struct linkedList *theLinkedList;
+    theLinkedList = (struct linkedList *)malloc(sizeof(struct linkedList));
+    theLinkedList->val = randnum(1, 10);
+    head = theLinkedList;
+    head->next = NULL;
+
+    int i;
+    for (i = 0; i < 3; i++) {
+        pthread_create(&searchThread[i], NULL, searcher, NULL);
+        pthread_create(&insertThread[i], NULL, inserter, NULL);
+        pthread_create(&deleteThread[i], NULL, deleter, NULL);
+    }
+
+    for (i = 0; i < 3; i++) {
+        pthread_join(searchThread[i], NULL);
+        pthread_join(insertThread[i], NULL);
+        pthread_join(deleteThread[i], NULL);
+    }
+
+    return 0;
 }
